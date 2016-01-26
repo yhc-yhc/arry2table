e36488a946431615a03d56c0a42e061e5f94f75c
yhc-yhc <2435071576@qq.com>
2016-01-26 13:20:18 +0800

    Update arry2table.js

--- arry2table.js
+++ arry2table.js
@@ -1,67 +1,85 @@
-'use strict';
-		var ary=[];
-        //create target array
-		for(var i = 1; i < 55; i++){
-			var obj = {};
-			obj.name = "name"+i;
-			obj.wxh = "wxh"+i;
-            		obj.aa = "ff";
-			ary.push(obj);
-		}
+function array2table(paramObj){
+    'use strict';
+        //valadate the param fun 
+        function getType(o) { 
+            var _t;
+            return ((_t = typeof(o)) == "object" ? o==null && "null" ||  Object.prototype.toString.call(o).slice(8,-1):_t).toLowerCase(); 
+        } 
+        if(getType(paramObj) !='object'){
+            console.log('the param is not current!');
+            return;
+        }
 
+        let ary= paramObj.dataSource || [];
+        let elemt = paramObj.tarElmt || null;
+        let tbClass = paramObj.tbClass || '';
+        let showIdx = paramObj.showIdx || false;
+        let callbk = paramObj.callbk || [];//every td of the oprations
+        let length = ary.length;
+
+        if (!length ) {
+            console.log('ary has no object!');
+            return;
+        }
+
+        if (!elemt ) {
+            console.log('the element of the table is not set!');
+            return;
+        }
         // get the object array's key 
-        var keys = ary.length ? Object.keys(ary[0]) : (function(){console.log('array has no object')})();
+        let keys = Object.keys(ary[0]);
 
-        if (keys) {
-            //create target table
-            var table = document.createElement("table");
+        //create target table
+        let table = document.createElement("table");
 
-            //set the class for the new create table
-            table.setAttribute('class', 'table table-striped table-responsive');
+        //set the class for the new create table
+        table.setAttribute('class', tbClass);
 
-            //set the head of the table 
-            let thead = document.createElement("thead");
-            let tr = document.createElement("tr");
+        //set the head of the table 
+        let thead = document.createElement("thead");
+        let tr = document.createElement("tr");
 
+        if(showIdx){
             let indxTh = document.createElement("th");
             let rowNum = document.createTextNode('index');
             indxTh.appendChild(rowNum);
             tr.appendChild(indxTh);
-
-            thead.appendChild(tr);
-
-            //create th for tr
-            for (let key of keys) {
-                let th = document.createElement("th");
-                let value = document.createTextNode(key);
-                th.appendChild(value);
-                tr.appendChild(th);
-            }
-            table.appendChild(thead);
-
-            //set the body of the table 
-            //@important : the table must has tbody
-            let tbody = document.createElement("tbody");
-            table.appendChild(tbody); 
-
-            //create tr for tbdoy 
-        		ary.forEach(function(element, index){
-                    let tr = document.createElement("tr");
-    
-                    let indxTd = document.createElement("td");
-                    let rowNum = document.createTextNode(index+1);
-                    indxTd.appendChild(rowNum);
-                    tr.appendChild(indxTd);
-    
-                    //create td for tr 
-                    for (let key of keys) {
-                        let td = document.createElement("td");
-                        let value = document.createTextNode(element[key]);
-                        td.appendChild(value);
-                        tr.appendChild(td);
-                    }
-    
-                    tbody.appendChild(tr);
-        		});
-            
         }
+
+        //create th for tr
+        for (let key of keys) {
+            let th = document.createElement("th");
+            let value = document.createTextNode(key);
+            th.appendChild(value);
+            tr.appendChild(th);
+        }
+        thead.appendChild(tr);
+        table.appendChild(thead);
+
+        //set the body of the table 
+        // @important : the table must has tbody
+        let tbody = document.createElement("tbody");
+        table.appendChild(tbody); 
+
+        //create tr for tbdoy 
+        ary.forEach(function(element, index){
+            let tr = document.createElement("tr");
+
+            if (showIdx) {
+                let indxTd = document.createElement("td");
+                let rowNum = document.createTextNode(index+1);
+                indxTd.appendChild(rowNum);
+                tr.appendChild(indxTd);
+            }
+
+            //create td for tr 
+            for (let key of keys) {
+                let td = document.createElement("td");
+                let value = document.createTextNode(element[key]);
+                td.appendChild(value);
+                tr.appendChild(td);
+            }
+            tbody.appendChild(tr);
+        });
+        elemt.appendChild(table);
+    }
